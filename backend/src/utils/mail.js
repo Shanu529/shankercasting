36761eeccb2fa6f9d289f324e22c1c38cbd712aa
Shanu Chhetri <shnau.chhetri529@gmail.com>
2026-02-19 15:41:05 +0530

    ..

--- backend/src/utils/mail.js
+++ backend/src/utils/mail.js
@@ -43,10 +43,22 @@
 
 
 
+// import nodemailer from "nodemailer";
+
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.MAIL_USER,
+//     pass: process.env.MAIL_PASS,
+//   },
+// });
+
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
